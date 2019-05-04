2c010ff65b0e43c2318908bf5255e51a64e7d1fd
SamuelMoriarty <mori@reu.moe>
2019-05-04 22:57:47 +0400

    Try again again

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,7 +18,7 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --manifest-path ceres-utils/Cargo.toml --bin ceres --release -- -C lto
+    cross rustc --manifest-path ceres-utils/Cargo.toml --bin ceres --release --target $TARGET -- -C lto
 
     # TODO Update this to package the right artifacts
     cp target/$TARGET/release/ceres $stage/
